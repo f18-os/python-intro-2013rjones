e540dcb87628680343341d056f5114587b89da6d
Ryanj <2013RJONES@GMAIL.COM>
2018-09-01 21:35:45 -0600

    Modified code to use any arguments passed in, handled error message.

--- wordCounter.py
+++ wordCounter.py
@@ -1,7 +1,8 @@
 import re
+import sys
 
-def printAll(wordDict):
-    outFile = open("myWordCntOutput.txt", "w+") # output file restarting if the file exists
+def printAll(wordDict,outFile):
+    outFile = open(outFile, "w+") # output file restarting if the file exists
     for word in sorted(wordDict): #pulls them out alphabetically sorted. 
         print word + " " + str(wordDict[word])
 
@@ -17,9 +18,22 @@ def processFileLines(fileName):
                     wordCounts[cleanWord] = wordCounts[cleanWord] + 1 
             else:
                     wordCounts[cleanWord] = 1
-    
-    printAll(wordCounts)
+   
+    return wordCounts
 
-processFileLines("speech.txt")
+def processInputArguments(args):
+    #making a strong assumption that we are always given a input followed by the output 
+    if len(args) < 3: 
+        print("Not enough arguments to run, should contain an input file name and an output file name.")
+    else:      
+        return "true" 
+
+
+if processInputArguments(sys.argv) == "true":
+    inputFile = sys.argv[1] 
+    outptFile = sys.argv[2]
+    printAll(processFileLines(inputFile), outptFile)
+
+print("End Program")
 
    

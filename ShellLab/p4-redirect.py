7f89605108896f9eef2cad17611d0924ecfe9162
Ryanj <2013RJONES@GMAIL.COM>
2018-09-10 12:10:29 -0600

    commit new files remove old unneeded.

--- ShellLab/p4-redirect.py
+++ ShellLab/p4-redirect.py
@@ -1,10 +1,8 @@
 #! /usr/bin/env python3
-#Majority of code taken from code given by Dr. Freudenthal.
-#Takes cmd, inputFile, Output file.
 
 import os, sys, time, re
 
-pid = os.getpid()  # get and remember pid
+pid = os.getpid()               # get and remember pid
 
 os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
 
@@ -15,10 +13,12 @@ if rc < 0:
     sys.exit(1)
 
 elif rc == 0:                   # child
-    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
+                 (os.getpid(), pid)).encode())
     args = ["wc", "p3-exec.py"]
+
     os.close(1)                 # redirect child's stdout
-    sys.stdout = open("sampOut.txt", "w")
+    sys.stdout = open("p4-output.txt", "w")
     fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
     os.set_inheritable(fd, True)
     os.write(2, ("Child: opened fd=%d for writing\n" % fd).encode())
@@ -26,7 +26,7 @@ elif rc == 0:                   # child
     for dir in re.split(":", os.environ['PATH']): # try each directory in path
         program = "%s/%s" % (dir, args[0])
         try:
-            os.execve(program, argpyths, os.environ) # try to exec program
+            os.execve(program, args, os.environ) # try to exec program
         except FileNotFoundError:             # ...expected
             pass                              # ...fail quietly 
 
@@ -34,6 +34,8 @@ elif rc == 0:                   # child
     sys.exit(1)                 # terminate with error
 
 else:                           # parent (forked ok)
-    os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %(pid, rc)).encode())
+    os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
+                 (pid, rc)).encode())
     childPidCode = os.wait()
-    os.write(1, ("Parent: Child %d terminated with exit code %d\n" %childPidCode).encode())
+    os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
+                 childPidCode).encode())

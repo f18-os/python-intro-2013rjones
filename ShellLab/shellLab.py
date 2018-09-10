dbc943c3f41282955f234c19e2108d193114202b
Ryanj <2013RJONES@GMAIL.COM>
2018-09-10 12:23:32 -0600

    Added proper call to p4-redirect

--- ShellLab/shellLab.py
+++ ShellLab/shellLab.py
@@ -30,7 +30,7 @@ while (cont == "True"):
                redirIn = "true"
        #should have all my information for a redirect by this point. 
        #now we should call the function passing arguments. 
-       os.system("python3 p4-redirect " + primeCmd + " " + inputFile + " " + outputFile)  
+       os.system("python3 p4-redirect.py " + primeCmd + " " + inputFile + " " + outputFile)  
        curCnt = 0
 
                   

04515600cecd4e08741316e46f56298f3d014b9f
Ryan Jones(VM) <2013rjones@gmail.com>
2018-09-17 13:45:16 -0600

    Modified redirect to no longer throw exception error due to stdin

--- Shell/p4-redirect.py
+++ Shell/p4-redirect.py
@@ -4,28 +4,32 @@
 #os.system("python3 p4-redirect.py -c " + primeCmd + " -i " + inputFile + " -o " + outputFile + " -E ")  
 
 import os, sys, time, re
+
 outputFile= ""
 inputFile = ""
 cmd = ""
 curDir = ""
 cmdDir = ""
-def argProcessing():
-    prev = ""
-    for x in argv[1:]:
-        if((prev.strip() == "-c") and (x.strip() != "-i")): 
-            cmd = x.strip() 
+myArgs = ""
+normIn = ""
+normOut = ""
+errOut = ""
+prev = ""
+for x in sys.argv[1:]:
+    if((prev.strip() == "-c") and (x.strip() != "-i")): 
+        cmd = x.strip() 
+    
+    if((prev.strip() == "-i") and (x.strip() != "-o")): 
+        inputFile = x.strip() 
         
-        if((prev.strip() == "-i") and (x.strip() != "-o")): 
-            inputFile = x.strip() 
-        
-        if((prev.strip() == "-o") and (x.strip() != "-cmdp")): 
-            outputFile = x.strip() 
-        if((prev.strip() == "-cmdp") and (x.strip() != "-cuP")): 
-            cmdDir = x.strip()     
-        if((prev.strip() == "-cuP") and (x.strip() != "-E")): 
-            curDir = x.strip()     
-        
-        prev = x  
+    if((prev.strip() == "-o") and (x.strip() != "-cmdp")): 
+        outputFile = x.strip()
+    if((prev.strip() == "-cmdp") and (x.strip() != "-cuP")): 
+        cmdDir = x.strip()     
+    if((prev.strip() == "-cuP") and (x.strip() != "-E")): 
+        curDir = x.strip()     
+    
+    prev = x  
 
 
 pid = os.getpid()               # get and remember pid
@@ -41,9 +45,9 @@ if rc < 0:
 elif rc == 0:                   # child
     #os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
     #print(sys.argv)
-    os.chdir(curDir)
     
-    args = [sys.argv[1], sys.argv[2]]
+    os.chdir(curDir)
+    args = [sys.argv[2], sys.argv[4]]
 
     normOut = sys.stdout
     normIn = sys.stdin
@@ -52,29 +56,31 @@ elif rc == 0:                   # child
     if(outputFile != ""):
         
         os.close(1) # redirect child's stdout
-        sys.stdout = open(sys.argv[3], "w")
+        sys.stdout = open(outputFile, "w")
         fd = sys.stdout.fileno() # os.open("p4-output.txt", os.O_CREAT)
         os.set_inheritable(fd, True)
     
-    if(outputFile != ""):
+    if(inputFile != ""):
         os.close(0)
-        sys.stdin = open(sys.argv[2])
+        sys.stdin = open(inputFile)
         fd1 = sys.stdin.fileno() # os.open("p4-output.txt", os.O_CREAT)
         os.set_inheritable(fd1, True)
-    os.chdir(curDir)
+    #os.chdir(cmdDir)
     for dir in re.split(":", os.environ['PATH']): # try each directory in path
         program = "%s/%s" % (dir, args[0])
         try:
-            os.execve(program, args + " -d " + curDir, os.environ) # try to exec program
+            os.execve(program, args, os.environ) # try to exec program
         except FileNotFoundError:             # ...expected
             pass                              # ...fail quietly 
-    os.chdir(curDir)
+    #os.chdir(curDir)
+    sys.stdout = normOut 
+    sys.stdin = normIn 
     os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
     sys.exit(1)                 # terminate with error
 
 else:                           # parent (forked ok)
     #os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %(pid, rc)).encode())
     childPidCode = os.wait()
+
     #os.write(1, ("Parent: Child %d terminated with exit code %d\n" %childPidCode).encode())
-sys.stdin = normIn
-sys.stdout = normOut
+    

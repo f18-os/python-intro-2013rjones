0f9e1abf687a6e72fd4ef1101074750905b7febb
Ryan Jones(VM) <2013rjones@gmail.com>
2018-09-17 12:37:21 -0600

    Modified to modify current working directory

--- Shell/shellLab.py
+++ Shell/shellLab.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import os
 # THIS WILL ONLY WORK FOR ONE PIPE CMD at a time. 
 
@@ -6,7 +8,9 @@ cont = True
 inputFile = " "; 
 outputFile = " "; 
 primeCmd = " "; 
-curDir = os.environ['PATH']
+curDir = os.path.dirname(os.path.realpath(__file__))
+origDir = curDir
+cmdDir= curDir
 while (cont):
    #print(curDir)
    commandFull = raw_input("$")
@@ -60,13 +64,58 @@ while (cont):
        #should have all my information for a redirect by this point. 
        #now we should call the function passing arguments. 
        if((redirIn == "true") or (redirOut == "true")):
-          os.system("python3 p4-redirect.py -c " + primeCmd + " -i " + inputFile + " -o " + outputFile + " -E ")  
+          os.system("python3 p4-redirect.py -c " + primeCmd + " -i " + inputFile + " -o " + outputFile + " -cmdp " + cmdDir + " -cuP " + curDir +  " -E " )  
           proceed = False
        if(proceed): #then that means there were no major changes to be made. 
-           #if((allCmdsString.strip() == "cd")): 
-               
-           os.system("python3 p3-exec.py " + allCmdsString)  
-           
+           execute = True
+           if((allCmdsString.strip() == "cd..")):
+               execute = False
+               try: 
+                   #print("Start: " + curDir)
+                   allDirect = curDir.split("/")
+                   #print("before rem: "+allDirect[-1])
+                   find = True
+                   cnt = -1 
+                   while(find):
+                       if(allDirect[cnt] != ""):
+                           find = False
+                       else:
+                           cnt = cnt - 1
+                           
+                   allDirect[cnt] = "" #blank last directory position
+                   
+                   #print("after rem: "+allDirect[-1])
+                   combineNewDirect= ""
+                   curCnt = 0 
+                   for each in allDirect:
+                        if(curCnt != 0):
+                            if(each != ""):
+                               combineNewDirect += "/" + each
+                        else:
+                            combineNewDirect +=  each
+                            curCnt = 1 
+                   #print("end:" +combineNewDirect)     
+                   os.chdir(combineNewDirect)
+                   curDir = combineNewDirect
+                   origDir = curDir
+               except: 
+                   print("Unable to change directory")
+                   os.chdir(origDir)
+           allSplit = allCmdsString.split()         
+           if((allSplit[0] == "cd")):
+               execute = False
+               curDir += "/" + allSplit[1]
+               try: 
+                    os.chdir(curDir)
+               except: 
+                    print("Unable to change directory")
+                    os.chdir(origDir)
+                    curDir = origDir 
+           if(execute):
+               #print("Current: " +curDir)
+               os.chdir(cmdDir)
+               os.system(" python3 p3-exec.py " + allCmdsString + " -d " + curDir)
+               os.chdir(curDir)        
         #okay now we need to reset to allow multiple runs.
                   
                 

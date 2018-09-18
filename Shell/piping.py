6d7940c2f97aca7ca5ea31e57f93ac5c394f0de0
Ryan Jones(VM) <2013rjones@gmail.com>
2018-09-17 20:13:29 -0600

    Modified code to properly pipe excluding chaining with redirecting

--- Shell/piping.py
+++ Shell/piping.py
@@ -1,48 +1,11 @@
 #! /usr/bin/env python3
+#called like so:  os.system("python3 piping.py " + " -fh " + firstHalf + " -sh " + secondHalf + " -E ") 
 
 import os, sys, time, re
 
-pid = os.getpid() # get and remember pid
-
-Pipefds = pipe() #returns two new file descriptors. 
-os.set_inheritable(Pipefds[0], True)
-os.set_inheritable(Pipefds[1], True)
-#os.write(1, ("About to fork (pid=%d)\n" % pid).encode())
-#now close fd 1 
-normOut = sys.stdout
-normIn = sys.stdin
-
-#split passed commands here
-# NEED TO ADD 
-#ADD REDIRECT CALL IF NECESSARY 
-
-#first child
-rc = os.fork()
-
-if rc < 0:
-    #os.write(2, ("fork failed, returning %d\n" % rc).encode())
-    sys.exit(1)
-
-elif rc == 0:                   # child
-    #os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
-    os.close(1)
-    dup(Pipefds[1]) #changed the output to our pipe.
-
-    fd1 = sys.stdin.fileno() # os.open("p4-output.txt", os.O_CREAT)
-    os.set_inheritable(fd1, True)
-
-    for dir in re.split(":", os.environ['PATH']): # try each directory in path
-        program = "%s/%s" % (dir, args[0])
-        try:
-            os.execve(program, args, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly 
-
-    os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-    sys.exit(1)                 # terminate with error
 
 
-#SECOND CHILD FOR THE OUTPUT
+r,w = os.pipe()
 
 rc = os.fork()
 
@@ -50,27 +13,23 @@ if rc < 0:
     #os.write(2, ("fork failed, returning %d\n" % rc).encode())
     sys.exit(1)
 
-elif rc == 0:                   # child
-    #os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % (os.getpid(), pid)).encode())
-    os.close(0)
-    dup(Pipefds[0]) #changed the output to our pipe.
+elif rc == 0:
+    os.close(r)
+    os.dup2(w, sys.stdout.fileno())
+    args = [sys.argv[2], sys.argv[3]]
     
-    for dir in re.split(":", os.environ['PATH']): # try each directory in path
-        program = "%s/%s" % (dir, args[0])
-        try:
-            os.execve(program, args, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly 
+    os.system(sys.argv[2] + " " + sys.argv[3])
+    sys.exit(1)     # terminate with error
 
-    os.write(2, ("Child:    Error: Could not exec %s\n" % args[0]).encode())
-    sys.exit(1)                 # terminate with error
-
-else:                           # parent (forked ok)
-    #os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %(pid, rc)).encode())
-    childPidCode = os.wait()
-    #os.write(1, ("Parent: Child %d terminated with exit code %d\n" %childPidCode).encode())
     
-os.close(3)
-os.close(4)
-sys.stdin = normIn
-sys.stdout = normOut
+rc2 = os.fork()
+if rc2 < 0:
+    #os.write(2, ("fork failed, returning %d\n" % rc).encode())
+    sys.exit(1)
+
+elif rc2 == 0:
+    os.close(w)
+    os.close(0) # redirect child's stdin
+    os.dup2(r, sys.stdin.fileno())
+    os.system(sys.argv[5]) 
+    sys.exit(1)     # terminate with error
